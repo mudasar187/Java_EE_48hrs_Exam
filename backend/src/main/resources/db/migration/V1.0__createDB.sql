4b169eb4982a7808d71e1da2d978f81627085eb2
mudasar187 <ahmmud16@hotmail.com>
2018-04-17 18:59:47 +0200

    deleted content in V1.0__createDB.sql, insert when get output sql, both .yml files to create-drop

--- backend/src/main/resources/db/migration/V1.0__createDB.sql
+++ backend/src/main/resources/db/migration/V1.0__createDB.sql
@@ -1,6 +1,1 @@
 create sequence hibernate_sequence start with 1 increment by 1;
-
-create table user_roles (user_username varchar(255) not null, roles varchar(255));
-create table users (username varchar(255) not null, enabled boolean not null, password varchar(255) not null, primary key (username));
-
-alter table user_roles add constraint FKs9rxtuttxq2ln7mtp37s4clce foreign key (user_username) references users;
